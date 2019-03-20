1309869d847d07eff46122dac61519550807a84a
Sammy Wu <swucode@gmail.com>
2019-03-20 19:04:51 +0000

    Automatically backed up by Learn

--- db/001_create_shows.rb
+++ db/001_create_shows.rb
@@ -1,12 +1,11 @@
+class CreateShows < ActiveRecord::Migration
 
-
-create_table :shows do |s|
-
-#name, networkk, day, rating COLUMNS 
-
-s.string :name 
-s.string :network 
-s.string :day
-s.integer :rating 
-
-end 
+  def change
+    create_table :shows do |s|
+      s.string :name
+      s.string :network
+      s.string :day
+      s.integer :rating
+    end
+  end
+end
