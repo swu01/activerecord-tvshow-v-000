f5d1e00f4fb31d467aa1f96bcba1866baeb995a0
Sammy Wu <swucode@gmail.com>
2019-03-20 19:06:52 +0000

    Automatically backed up by Learn

--- db/002_add_season_to_shows.rb
+++ db/002_add_season_to_shows.rb
@@ -1,16 +1,7 @@
-class AddSeasonToShows << ActiveRecord::Migration 
+class AddSeasonToShows < ActiveRecord::Migration 
   
   def change
     add_column :shows, :season, :string  
   end 
 end 
   
-  
-  
-class AddSeasonToShows < ActiveRecord::Migration
-
-  def change
-    add_column :shows, :season, :string
-  end
-
-end
