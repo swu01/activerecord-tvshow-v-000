7e3ac98d080e10f5a3f683daf29fe51a88643468
Sammy Wu <swucode@gmail.com>
2019-03-20 19:17:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,23 +1,23 @@
-class Show < ActiveRecord::Base 
-  
-  def self.highest_rating 
-    Show.maximum(:rating) 
-  end 
-  
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
   def self.most_popular_show
     rating_num = self.highest_rating
     Show.find_by(rating: rating_num)
   end
-  
+
   def self.lowest_rating
     Show.minimum(:rating)
-  end 
-  
+  end
+
   def self.least_popular_show
     rating_num = self.lowest_rating
     Show.find_by(rating: rating_num)
-  end 
-  
+  end
+
   def self.ratings_sum
     Show.sum(:rating)
   end
@@ -29,5 +29,4 @@ class Show < ActiveRecord::Base
   def self.shows_by_alphabetical_order
     Show.order(:name)
   end
-  
 end
